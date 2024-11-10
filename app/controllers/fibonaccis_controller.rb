d3a952e8880954994f57056cc8e8346b6e0c28c4
n7n788 <yuki1113.2001@gmail.com>
2024-11-10 17:46:28 +0900

    add: フィボナッチコントローラを作成

---
+++ app/controllers/fibonaccis_controller.rb
@@ -0,0 +1,33 @@
+class FibonaccisController < ApplicationController
+
+  # GET /fib
+  def show
+    # 整数でない場合
+    unless is_integer?(n_params)
+      render json: { status: 400, message: "Bad request." }, status: 400
+      return
+    end
+
+    n = n_params.to_i - 1
+    if n < 0 # 負の整数の場合
+      render json: { status: 400, message: "Bad request." }, status: 400
+    else
+      render json: { result: Fibonacci.value(position: n) }, status: 200
+    end
+  end
+
+  private
+
+  def n_params
+    params.require(:n)
+  end
+
+  def is_integer?(n)
+    case n
+    when /\A-?\d+\z/
+      true
+    else
+      false
+    end
+  end
+end
