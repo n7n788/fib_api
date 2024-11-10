215c34caa3bfd6b240be61bd009bd9987b5f6c5a
n7n788 <yuki1113.2001@gmail.com>
2024-11-10 18:39:45 +0900

    delete: CONSTRAINTを削除

--- db/migrate/20241110044911_create_fibonaccis.rb
+++ db/migrate/20241110044911_create_fibonaccis.rb
@@ -6,6 +6,5 @@ class CreateFibonaccis < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
-    execute "ALTER TABLE fibonaccis ADD CONSTRAINT position_non_negative CHECK (position >= 0)"
   end
 end
