274239c4557468f9b341deb85e308156e934f2bb
n7n788 <yuki1113.2001@gmail.com>
2024-11-16 17:31:52 +0900

    delete: fibonaccisのリクエストスペックから不要なテストを削除

--- spec/requests/fibonaccis_spec.rb
+++ spec/requests/fibonaccis_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'Fibonaccis', type: :request do
   describe '#GET /fib' do
-    context 'when params n is positive integer' do
+    context 'when params n is 1' do
       it 'returns http 200 status' do
         get '/fib', params: { n: 1 }
         expect(response).to have_http_status(200)
@@ -25,22 +25,6 @@ RSpec.describe 'Fibonaccis', type: :request do
 
       it 'returns json response' do
         get '/fib', params: { n: 0 }
-        response_body = JSON.parse(response.body)
-        aggregate_failures do
-          expect(response_body['status']).to eq 400
-          expect(response_body['message']).to eq 'Bad request.'
-        end
-      end
-    end
-
-    context 'when params n is negative integer' do
-      it 'returns http 400 status' do
-        get '/fib', params: { n: -1 }
-        expect(response).to have_http_status(400)
-      end
-
-      it 'returns json response' do
-        get '/fib', params: { n: -1 }
         response_body = JSON.parse(response.body)
         aggregate_failures do
           expect(response_body['status']).to eq 400
